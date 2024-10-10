f3ea190dde0df57628593063373cd71add45dc6d
Sai Tarun Tavva <saitarun.tavva@gmail.com>
2024-10-10 14:58:48 -0400

    feat: add overlay message

---
+++ src/Components/OverlayMessage/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./index.module.scss";
+
+const OverlayMessage = () => {
+  return (
+    <div className={classes.overlay}>
+      <div className={classes.content}>
+        <h1 className={classes.title}>
+          Elevate Your <span className={classes.highlight}>Career</span>
+        </h1>
+        <p className={classes.subtitle}>AI-powered resume analysis awaits</p>
+        <div className={classes.rocket}>
+          <i className="bi bi-rocket-takeoff"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+OverlayMessage.displayName = "OverlayMessage";
+export default OverlayMessage;
--- src/Components/SparkHub/index.jsx
+++ src/Components/SparkHub/index.jsx
@@ -1,12 +1,14 @@
 import Operations from "../Operations";
-import Results from "../Results";
+import OverlayMessage from "../OverlayMessage";
+// import Results from "../Results";
 import classes from "./index.module.scss";
 
 const SparkHub = () => {
   return (
     <div className={classes.sparkHub}>
       <Operations />
-      <Results />
+      {/* <Results /> */}
+      <OverlayMessage />
     </div>
   );
 };
