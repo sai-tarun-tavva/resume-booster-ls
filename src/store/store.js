955d751c43ddb18bc306ae7cb81ec3ee4c3f8f05
Sai Tarun Tavva <saitarun.tavva@gmail.com>
2024-10-14 15:42:58 -0400

    feat: add status message

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import dataReducer from "./data";
 import loadingReducer from "./loading";
 import resultReducer from "./result";
+import statusReducer from "./status";
 
 // Combining reducers into a single reducer object
 const reducer = {
   data: dataReducer, // Manages user input data
   result: resultReducer, // Manages results from API responses
   loading: loadingReducer, // Manages loading state during data fetching
+  status: statusReducer, // Manages status message for api errors
 };
 
 // Configuring the Redux store with the combined reducers
